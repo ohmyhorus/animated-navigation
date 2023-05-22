c998b717b7491eeb73c6c07cce27bd8bfb464a90
Sasha <45601549+ohmyhorus@users.noreply.github.com>
2023-05-22 19:27:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.firstElementById("overlay");
+const nav1 = document.getElementById("nav1");
+const nav2 = document.getElementById("nav2");
+const nav3 = document.getElementById("nav3");
+const nav4 = document.getElementById("nav4");
+const nav5 = document.getElementById("nav5");
+
+function toggleNav() {
+  menuBars.classList.toggle("change");
+}
+
+//event listeners
+menuBars.addEventListener("click", toggleNav);
+nav1.addEventListener("click", toggleNav);
+nav2.addEventListener("click", toggleNav);
+nav3.addEventListener("click", toggleNav);
+nav4.addEventListener("click", toggleNav);
+nav5.addEventListener("click", toggleNav);
